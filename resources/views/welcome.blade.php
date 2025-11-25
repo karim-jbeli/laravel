60de02dc283851385f1b8f9a03fd27fcd0bf300a
ghofraneamara <ghofraneamara20@gmail.com>
2025-11-25 17:38:15 +0100

    modifier par salim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,7 +125,7 @@
                                     <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts</h2>
 
                                     <p class="mt-4 text-sm/relaxed">
-                                        karimes offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    salim offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                                     </p>
                                 </div>
 
