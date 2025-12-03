b63934b3e91941c2a6bfb75be33ab4e5944ae12e
ghofraneamara <ghofraneamara20@gmail.com>
2025-12-03 19:09:14 +0100

    changer par go

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-<h1>changement </h1>
+<h1>changement par ghofr</h1>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
